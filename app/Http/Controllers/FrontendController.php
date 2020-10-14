f1107e8d753d14b83f4ba31d6e9e86adab637498
Shuvo <shuvonaim123@gmail.com>
2020-10-14 22:31:56 +0600

    contact page problem solved

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -23,31 +23,38 @@ class FrontendController extends Controller
 
         $cart_products_numbers =Cart::where('random_number', session('random_number'))->count();
 
-        
+
         $products = Product::orderBy('id','desc')->get();
         return view('frontend.index', compact('categories', 'products', 'carts', 'cart_products_numbers'));
     }
 
     public function Contact(){
         $categories = Category3::all();
+        $carts = DB::table('carts')
+        ->join('products', 'carts.product_id', '=', 'products.id')
+        ->select('carts.*', 'products.image as product_image', 'products.name as product_name')
+        ->where('random_number', session('random_number'))
+        ->orderBy('id', 'desc')
+        ->get();
+        $cart_products_numbers =Cart::where('random_number', session('random_number'))->count();
          // $carts = Cart::where('random_number', session('random_number'))->orderBy('id', 'desc')->get();
         $maps = Contact::all();
-        return view('frontend.contact', compact('maps', 'categories'));
-        
+        return view('frontend.contact', compact('maps', 'categories', 'carts', 'cart_products_numbers'));
+
     }
     public function Blog(){
         $categories = Category3::all();
          // $carts = Cart::where('random_number', session('random_number'))->orderBy('id', 'desc')->get();
         return view('frontend.blog', compact('categories'));
-        
+
     }
 
     public function shop()
-    { 
+    {
         $categories = Category3::all();
-         
+
         return view('frontend.shop', compact('categories'));
     }
 
- 
+
 }
